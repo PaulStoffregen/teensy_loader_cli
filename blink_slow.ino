a96429b2cc9eadad9496d8f8770361f8948666bb
PaulStoffregen <paul@pjrc.com>
2019-10-08 04:10:15 -0700

    Update example blink HEX files

---
+++ blink_slow.ino
@@ -0,0 +1,15 @@
+// Simple LED blink
+
+const int led = LED_BUILTIN;
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
+}
+
